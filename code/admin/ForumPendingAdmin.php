1590741fb446a7062269b0c57fec53b309668334
Stewart Wilson <stewartwilson@outlook.com>
2015-05-21 15:49:37 +1000

    Modeladmin for pending users done

--- code/admin/ForumPendingAdmin.php
+++ code/admin/ForumPendingAdmin.php
@@ -7,22 +7,58 @@ class ForumPendingAdmin extends ModelAdmin {
 	private static $menu_title  = 'Pending Forum';
 	private static $url_segment = 'forum';
 
-	private static $managed_models  = array('Post', 'Member');
+	private static $managed_models  = array('Post', 'Group');
 	private static $model_importers = array();
 	
 	public function getEditForm($id = null, $fields = null) {
 		$form = parent::getEditForm($id, $fields);
+	
+		$gridFieldName = $this->sanitiseClassName($this->modelClass);
+		$gridField = $form->Fields()->fieldByName($gridFieldName);
+	
+		// modify the list view.
+		$gridFieldConfig = $gridField->getConfig();
+	
+		$gridFieldConfig->removeComponentsByType('GridFieldAddNewButton');
+		$gridFieldConfig->removeComponentsByType('GridFieldPrintButton');
+		$gridFieldConfig->removeComponentsByType('GridFieldExportButton');
 		
 		if($this->modelClass == 'Post') {
-			$gridField = $form->Fields()->fieldByName("Post");
-		
-			// modify the list view.
-			$gridFieldConfig = $gridField->getConfig();
-		
-			$gridFieldConfig->removeComponentsByType('GridFieldAddNewButton');
-			$gridFieldConfig->removeComponentsByType('GridFieldPrintButton');
-			$gridFieldConfig->removeComponentsByType('GridFieldExportButton');
 			$gridFieldConfig->addComponent(new GridField_ApprovePostAction());
+		}
+		
+		if($this->modelClass == 'Group') {
+			//Remove the delete button
+			$gridFieldConfig->removeComponentsByType('GridFieldDeleteAction');
+			
+			// Modify the detail form
+			$detailForm = $gridFieldConfig->getComponentByType('GridFieldDetailForm');
+			$detailForm->setItemEditFormCallback(function($form, $component) {
+				$fields = $form->Fields();
+				$groupId = $fields->dataFieldByName('ID')->Value();
+				
+				$group = Group::get()->byID($groupId);
+				$members = $group->Members()->filter("Approved", false);
+
+				// Remove tabs
+				$fields->removeByName("Members");
+				$fields->removeByName("Permissions");
+				$fields->removeByName("Roles");
+				$fields->removeByName("Subsites");
+				$fields->removeByName("RestrictedArea");
+				$fields->removeByName("Forum");
+				
+				// GridFieldConfig
+				$gridFieldConfig = GridFieldConfig_RecordEditor::create();
+				$gridFieldConfig->removeComponentsByType('GridFieldAddNewButton');
+				$gridFieldConfig->getComponentByType('GridFieldDataColumns')->setDisplayFields(array ('FirstName'=>'FirstName','Surname'=>'Surname','Email'=>'Email', 'Approved' => 'Approved'));
+				$gridFieldConfig->addComponent(new GridField_ApproveUserAction());
+				
+				//$fields->addFieldToTab("Root.Members", HeaderField::create('Test'));
+				$fields->addFieldToTab("Root.Members", $gridField = new GridField('Members', 'Members', $members, $gridFieldConfig));
+				$gridField->setForm($form);
+				
+			});
 		}
 		
 		return $form;
@@ -36,6 +72,10 @@ class ForumPendingAdmin extends ModelAdmin {
 		// But if a search paramater is used, show according to that
 		if($this->modelClass == 'Post' && !(isset($params['Status']) && $params['Status'])) {
 			$list = $list->filter('Status', 'Awaiting');
+		}
+		
+		if($this->modelClass == 'Group') {
+			$list = $list->filter('IsForumGroup', true);
 		}
 	
 		return $list;
